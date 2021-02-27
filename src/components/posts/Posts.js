9c7a72968f9003a09461e4ddb472b26ee7d6ba1f
boldalex <46719335+boldalex@users.noreply.github.com>
2021-02-27 11:40:50 -0500

    bug_fixed

--- src/components/posts/Posts.js
+++ src/components/posts/Posts.js
@@ -1,6 +1,4 @@
 import React from "react";
-import { Box, Button } from '@material-ui/core';
-import { useEffect } from "react";
 
 import { useState } from "react";
 import CreatePostForm from "./CreatePostForm";
@@ -9,10 +7,6 @@ import Post from "./Post";
 const Posts = () => {
 
     const [posts, setPosts] = useState([]);
-
-    useEffect(()=> {
-        console.log(posts);
-    });
 
     const handleCreatePost = (newPost) => {
         setPosts([
