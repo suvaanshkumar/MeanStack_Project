43c21891d5cdb802b0a82514ade10ebc91dc280b
boldalex <46719335+boldalex@users.noreply.github.com>
2021-02-27 07:54:37 -0500

    initial commit

---
+++ src/components/Footer.js
@@ -0,0 +1,23 @@
+import { BottomNavigation, Box, Typography } from '@material-ui/core';
+import React from 'react';
+import MenuIcon from '@material-ui/icons/Menu';
+
+const Footer = (props) => {
+
+    return (
+        <div>
+                <Box display="flex" flexDirection="column" justifyContent="center">
+                    <Typography variant="h6" align="center" gutterBottom>
+                        Blog
+                    </Typography>
+                    <Typography variant="subtitle1" align="center" color="inherit" component="p">
+                        {'Copyright © 2021'}
+                    </Typography> 
+                </Box>
+
+        </div>
+    );
+
+};
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,34 @@
+import { AppBar, Button, IconButton, Toolbar, Typography } from '@material-ui/core';
+import React from 'react';
+import MenuIcon from '@material-ui/icons/Menu';
+
+const Header = (props) => {
+
+    return (
+        <div>
+            <AppBar position="static" className="header">
+                <Toolbar>
+                    <IconButton edge="start" color="inherit" aria-label="menu">
+                        <MenuIcon/>
+                    </IconButton>
+                    <Typography variant="h6" className="header">
+                        Blog
+                    </Typography>
+
+                    <span className="spacer"/>
+
+                    <Button color="inherit">Posts</Button>
+                    <Button color="inherit">Categories</Button>
+                    <Button color="inherit">Contact Us</Button>
+
+                    <span className="spacer"/>
+                    <Button color="inherit">Login</Button>
+                    <Button color="inherit">Signup</Button>
+                </Toolbar>
+            </AppBar>
+        </div>
+    );
+
+};
+
+export default Header;
