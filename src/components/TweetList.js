c0549c5d7f3db8be93eae6cbd01e4edec8b7f978
herman <hermanjustino@gmail.com>
2021-04-21 22:52:52 -0400

    Client final

--- src/components/TweetList.js
+++ src/components/TweetList.js
@@ -1,19 +1,28 @@
-import React, { Component } from 'react';
-import { FacebookProvider, Group } from 'react-facebook';
- 
-export default class TweetList extends Component {
-  render() {
-    return (
-      <FacebookProvider appId="470755350743457">
-        <Group
-          href="https://www.facebook.com/groups/1137699903050458"
-          width="300"
-          showSocialContext={true}
-          showMetaData={true}
-          skin="light"
-        />
-      </FacebookProvider>
-      
-    );
-  }
+import React from "react";
+import "./styles.css";
+import YoutubeEmbed from "./YoutubeEmbed";
+
+
+export default function TweetList() {
+  return (
+    <div>
+    <div className="App">
+      <h1>Sydney</h1>
+      <YoutubeEmbed embedId="4FdlOJicGBk" />
+    </div><span></span>
+    <div className="App">
+      <h1>LA</h1>
+      <YoutubeEmbed embedId="Co8Ljb4RLe4" />
+    </div>
+    <div className="App">
+      <h1>Tokyo</h1>
+      <YoutubeEmbed embedId="0nTO4zSEpOs" />
+    </div>
+    <div className="App">
+      <h1>Dubai</h1>
+      <YoutubeEmbed embedId="jB2Z9PA7iKI" />
+    </div>
+    </div>
+    
+  );
 }
