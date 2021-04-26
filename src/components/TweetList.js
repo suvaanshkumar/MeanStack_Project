a44cb1b3ee07a41fd558b8c7fa30daac12b1d0e5
herman <hermanjustino@gmail.com>
2021-04-26 17:03:25 -0400

    Update TweetList.js

--- src/components/TweetList.js
+++ src/components/TweetList.js
@@ -1,4 +1,61 @@
-import React from "react";
+class BodyData extends React.Component {
+  state = {
+    query: "",
+    data: [],
+    filteredData: []
+  };
+
+  handleInputChange = event => {
+    const query = event.target.value;
+
+    this.setState(prevState => {
+      const filteredData = prevState.data.filter(element => {
+        return element.name.toLowerCase().includes(query.toLowerCase());
+      });
+
+      return {
+        query,
+        filteredData
+      };
+    });
+  };
+
+  getData = () => {
+    fetch(`https://restcountries.eu/rest/v2/all`)
+      .then(response => response.json())
+      .then(data => {
+        const { query } = this.state;
+        const filteredData = data.filter(element => {
+          return element.name.toLowerCase().includes(query.toLowerCase());
+        });
+
+        this.setState({
+          data,
+          filteredData
+        });
+      });
+  };
+
+  componentWillMount() {
+    this.getData();
+  }
+
+  render() {
+    return (
+      <div className="searchForm">
+        <form>
+          <input
+            placeholder="Search for..."
+            value={this.state.query}
+            onChange={this.handleInputChange}
+          />
+        </form>
+        <div>{this.state.filteredData.map(i => <p>{i.name}</p>)}</div>
+      </div>
+    );
+  }
+}
+/*import React from "react";
 import "./styles.css";
 import YoutubeEmbed from "./YoutubeEmbed";
 
@@ -26,3 +83,4 @@ export default function TweetList() {
     
   );
 }
+*/
